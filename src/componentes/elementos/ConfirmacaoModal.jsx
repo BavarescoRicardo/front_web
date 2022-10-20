02b5abea56224ba269f5dac1578f05d9297ed1c9
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-10-19 22:28:53 -0300

    removidos botos fechar x do dialog para confirmar

--- src/componentes/elementos/ConfirmacaoModal.jsx
+++ src/componentes/elementos/ConfirmacaoModal.jsx
@@ -6,20 +6,20 @@ function ConfirmacaoModal(props){
     return(        
         <div>
             <div  role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Sair do usuário logado</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Sair do usuário logado</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" hidden="true">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <p>Tem certeza que deseja sair</p>
                         </div>
-                        <div class="modal-footer">
-                        <Link to="/" class="btn btn-primary">Fechar</Link>
-                            <Link to="/Login" class="btn btn-primary">Confirmar</Link>
+                        <div className="modal-footer">
+                        <Link to="/" className="btn btn-primary">Fechar</Link>
+                            <Link to="/Login" className="btn btn-primary">Confirmar</Link>
                         </div>
                     </div>
                 </div>
