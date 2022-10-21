49206845bed270f648f0b3a44c02b962dedafe0b
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-10-21 14:34:25 -0300

    ajustes estilo para elemnto confirmar sair

--- src/componentes/elementos/ConfirmacaoModal.jsx
+++ src/componentes/elementos/ConfirmacaoModal.jsx
@@ -5,21 +5,21 @@ import { Link } from 'react-router-dom'
 function ConfirmacaoModal(props){  
     return(        
         <div>
-            <div  role="dialog">
+            <div className="dialogo">
                 <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
+                    <div className="modal-content" style={{marginTop: '5%'}}>
+                        <div className="modal-header d-flex justify-content-center">
                             <h5 className="modal-title">Sair do usuário logado</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close" hidden="true">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div className="modal-body">
+                        <div className="modal-body d-flex justify-content-center">
                             <p>Tem certeza que deseja sair</p>
                         </div>
-                        <div className="modal-footer">
-                        <Link to="/" className="btn btn-primary">Fechar</Link>
-                            <Link to="/Login" className="btn btn-primary">Confirmar</Link>
+                        <div className="modal-footer d-flex justify-content-center">
+                        <Link to="/" className="btn btn-primary" style={{margin: '20px', width: '100px'}}>Fechar</Link>
+                            <Link to="/Login" className="btn btn-primary" style={{margin: '20px', width: '100px'}}>Confirmar</Link>
                         </div>
                     </div>
                 </div>
