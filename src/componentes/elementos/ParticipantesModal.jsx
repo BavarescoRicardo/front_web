09b89995fe953c92740187ef392cff1fff679c59
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-11-05 21:56:07 -0300

    ajuste participante modal para link ao perfil

--- src/componentes/elementos/ParticipantesModal.jsx
+++ src/componentes/elementos/ParticipantesModal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal';
+import { Link } from 'react-router-dom'
 
 function ParticipantesModal(props){  
 
@@ -14,7 +15,7 @@ function ParticipantesModal(props){
                 <Modal.Title>Participantes</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {props.users.map((number) =>  <li>{number}</li>)}                
+                    {props.users.map((nome) =>  <li> <Link to="/Perfil" className="nav-link" style={{color: '#4f7279'}}>{nome} </Link> </li>)}                
                 </Modal.Body>
                 <Modal.Footer>
 
